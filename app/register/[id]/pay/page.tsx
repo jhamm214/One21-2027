e1cfd89175d77f307aef0d01d3372a86bf43573d
jhamm214 <105677214+jhamm214@users.noreply.github.com>
2026-07-14 20:21:43 -0500

    Update page.tsx

--- app/register/[id]/pay/page.tsx
+++ app/register/[id]/pay/page.tsx
@@ -73,7 +73,8 @@ export default async function PayPage({
 
   const isInstallment = reg.plan === "installment";
 
- <PayForm
+  return (
+    <PayForm
       registrationId={reg.id}
       agentName={reg.agent_name}
       isInstallment={isInstallment}
